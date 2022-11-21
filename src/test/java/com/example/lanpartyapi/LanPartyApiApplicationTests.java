cffd513f2f1c0442c9c336a54b380135a6ddf6d2
Troels <troe1912@stud.kea.dk>
2022-11-21 12:54:58 +0100

    init commit

---
+++ src/test/java/com/example/lanpartyapi/LanPartyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lanpartyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanPartyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
