bd7bf14e34fc8cc104396058db9863ad3a663061
DoroHD <david.bjakobsen@gmail.com>
2022-11-22 12:37:08 +0100

    Add LanUser Entity, dto repository, service and unit tests

---
+++ src/main/java/com/example/lanpartyapi/dto/LanUserRequest.java
@@ -0,0 +1,12 @@
+package com.example.lanpartyapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LanUserRequest {
+
+    private String username;
+    private String password;
+}
--- src/test/java/com/example/lanpartyapi/LanPartyApiApplicationTests.java
+++ src/test/java/com/example/lanpartyapi/LanPartyApiApplicationTests.java
@@ -6,8 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LanPartyApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
 }
