602b75eb8fd958f71ec5ea64898d6072016642ec
Lasse Rasmussen <lass531n@stud.kea.dk>
2022-11-22 13:22:40 +0100

    cookie test

---
+++ src/main/java/com/example/lanpartyapi/api/LoginController.java
@@ -0,0 +1,31 @@
+package com.example.lanpartyapi.api;
+
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.Cookie;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/login")
+public class LoginController {
+    
+    @GetMapping()
+    public ResponseEntity<String> cookieTest (HttpServletResponse httpServletResponse){
+        Cookie cookie = new Cookie("testCookie", "test");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(60*60);
+        cookie.setPath("/");
+
+        httpServletResponse.addCookie(cookie);
+
+        return new ResponseEntity<>("Succesfully received cookie", HttpStatus.OK);
+    }
+
+}
