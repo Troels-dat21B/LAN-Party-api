0a464e0a9ad4d6fcd4067f8dba6c586828a6e5cb
Troels <troe1912@stud.kea.dk>
2022-11-23 11:25:14 +0100

    Lavet de første par teste for TablePlan og Segment

--- src/main/java/com/example/lanpartyapi/repository/TablePlanRepo.java
+++ src/main/java/com/example/lanpartyapi/repository/TablePlanRepo.java
@@ -3,5 +3,10 @@ package com.example.lanpartyapi.repository;
 import com.example.lanpartyapi.entity.TablePlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TablePlanRepo extends JpaRepository<TablePlan, Integer> {
+    TablePlan findByName(String name);
+
+
 }
