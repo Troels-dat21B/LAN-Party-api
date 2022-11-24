4573c9e564a760af9df30fd82db8132013e13cb7
Lasse Rasmussen <lass531n@stud.kea.dk>
2022-11-24 14:50:57 +0100

    Add Authorization endpoint

--- src/main/java/com/example/lanpartyapi/api/AuthController.java
+++ src/main/java/com/example/lanpartyapi/api/AuthController.java
@@ -3,6 +3,7 @@ package com.example.lanpartyapi.api;
 import com.example.lanpartyapi.dto.LanUserRequest;
 import com.example.lanpartyapi.service.AuthService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,6 +26,13 @@ public class AuthController {
     public ResponseEntity<HashMap<String, String>> signIn(@RequestBody LanUserRequest lanUserRequest){
         var token = this.authService.signIn(lanUserRequest);
 
-        return new ResponseEntity<>(new HashMap<>(Map.of("token", token)), HttpStatus.OK);
+        return new ResponseEntity<>(new HashMap<>(Map.of("accessToken", token)), HttpStatus.OK);
+    }
+
+    @PostMapping("/authorize")
+    public ResponseEntity<HashMap<String, String>> authorize(@RequestBody HashMap<String, String> payload) {
+        this.authService.authorize(payload.get("accessToken"));
+
+        return new ResponseEntity<>(new HashMap<>(Map.of("message", "Successful authorization")), HttpStatus.OK);
     }
 }
